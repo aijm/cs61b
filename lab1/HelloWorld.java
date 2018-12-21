cedb425bbbae68a6cb7ce695fd922fc2942e722c
Aijm <1907059378@qq.com>
2018-12-21 16:11:36 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    /** Return the larger of x and y. */
+    public static int larger(int x, int y){
+
+        if(x>y){
+            return x;
+        }else{
+            return y;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(larger(-5, 10));
+    }
+}
