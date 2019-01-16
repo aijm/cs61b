682f740d85a59c64ac78c32af0dd548541c87748
Aijm <1907059378@qq.com>
2019-01-16 20:34:25 +0800

    completed lab13: Radix sort MSD

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -31,7 +31,29 @@ public class RadixSort {
         return sorted;
     }
 
-    /** get the bucket of ith least significant bit of str
+    /**
+     * Does MSD radix sort on the passed in array with the following restrictions:
+     * The array can only have ASCII Strings (sequence of 1 byte characters)
+     * The sorting is stable and non-destructive
+     * The Strings can be variable length (all Strings are not constrained to 1 length)
+     *
+     * @param asciis String[] that needs to be sorted
+     *
+     * @return String[] the sorted array
+     */
+    public static String[] sort_MSD(String[] asciis) {
+        String[] sorted = asciis.clone();
+        int maxlen = 0;
+        for(String str : asciis){
+            if(str.length() > maxlen){
+                maxlen = str.length();
+            }
+        }
+        sortHelperMSD(sorted, 0, sorted.length, maxlen-1);
+        return sorted;
+    }
+
+    /** get the bucket of ith least significant digit of str
      * For example:
      * getbucket("bc", 0) == 99+1
      * getbucket("bc", 1) == 98+1
@@ -87,7 +109,43 @@ public class RadixSort {
      *
      **/
     private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
+        if(index == -1){
+            return;
+        }
         // Optional MSD helper method for optional MSD radix sort
-        return;
+        int[] count = new int[257]; // using count[0] for empty
+
+        for(int i = start;i < end;i++){
+            int bucket = getbucket(asciis[i], index);
+            count[bucket]++;
+        }
+        // update count to start position
+        int count_before = count[0];
+        count[0] = 0;
+        for(int i = 1;i < count.length;i++){
+            int temp = count[i];
+            count[i] = count[i-1] + count_before;
+            count_before = temp;
+        }
+        int[] startpos = count.clone();
+
+        // sorted using ith most significant digit
+        String[] asciis_copy = new String[end - start];
+        System.arraycopy(asciis,start,asciis_copy,0, end - start);
+        for(String item : asciis_copy){
+            int place = count[getbucket(item, index)] + start;
+            asciis[place] = item;
+            count[getbucket(item, index)]++;
+        }
+
+
+
+        // for the same most significant digit(can be considered as several frames)
+        // apply sortHelperMSD
+        for(int i = 0;i < count.length - 1;i++){
+            int frame_start = startpos[i] + start;
+            int frame_end = startpos[i+1] + start;
+            sortHelperMSD(asciis, frame_start, frame_end, index - 1);
+        }
     }
 }
--- lab13/RadixSortTester.java
+++ lab13/RadixSortTester.java
@@ -8,7 +8,7 @@ public class RadixSortTester {
     @Test
     public void testRadixSort(){
         String[] asciis = {"123","5","6","78","1300","854","45"};
-        String[] sorted = RadixSort.sort(asciis);
+        String[] sorted = RadixSort.sort_MSD(asciis);
         String[] expected = {"5","6","45","78","123","854","1300"};
         assertArrayEquals(expected, sorted);
 
@@ -28,6 +28,30 @@ public class RadixSortTester {
         }
         String[] sorted_string = new String[N];
         sorted_string = RadixSort.sort(asciis);
+        int[] sorted_int = new int[N];
+        for(int i = 0;i < N;i++){
+            sorted_int[i] = Integer.parseInt(sorted_string[i]);
+        }
+        int[] expected = numbers.clone();
+        Arrays.sort(expected);
+        assertArrayEquals(expected, sorted_int);
+    }
+
+    @Test
+    public void RandomTestRadixSort_MSD(){
+        int N = 1000;
+        int[] numbers = new int[N];
+        Random random = new Random();
+        for(int i = 0;i < N;i++){
+            numbers[i] = random.nextInt(1000);
+//            System.out.println(numbers[i]);
+        }
+        String[] asciis = new String[N];
+        for(int i = 0;i < N;i++){
+            asciis[i] = Integer.toString(numbers[i]);
+        }
+        String[] sorted_string = new String[N];
+        sorted_string = RadixSort.sort_MSD(asciis);
         int[] sorted_int = new int[N];
         for(int i = 0;i < N;i++){
             sorted_int[i] = Integer.parseInt(sorted_string[i]);
